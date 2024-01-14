12231b7817a6e639a3a335b159e9d6b1f0e62957
rydnr <github@acm-sl.org>
2024-01-14 17:36:57 +0100

    Added testcontainers-python dependency

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# flow-sample-tests
-Definition of pythoneda-sandbox/flow-sample-tests
+# pythoneda-sandbox/flow-sample-tests
+
+Definition of <https://github.com/pythoneda-sandbox/flow-sample-tests>.
+
+## How to run the tests
+
+``` sh
+nix run https://github.com/pythoneda-sandbox-def/flow-sample-tests
+```
+
+## How to declare it in your flake
+
+Check the latest tag of this repository and use it instead of the `[version]` placeholder below.
+
+```nix
+{
+  description = "[..]";
+  inputs = rec {
+    [..]
+    pythoneda-sandbox-flow-sample-tests = {
+      [optional follows]
+      url =
+        "github:pythoneda-sandbox-def/flow-sample-tests/[version]";
+    };
+  };
+  outputs = [..]
+};
+```
+
+Should you use another PythonEDA modules, you might want to pin those also used by this project. The same applies to [nixpkgs](https://github.com/nixos/nixpkgs "nixpkgs") and [flake-utils](https://github.com/numtide/flake-utils "flake-utils").
+
+Use the specific package depending on your system (one of `flake-utils.lib.defaultSystems`) and Python version:
+
+- `#packages.[system].pythoneda-sandbox-flow-sample-tests-python38` 
+- `#packages.[system].pythoneda-sandbox-flow-sample-tests-python39` 
+- `#packages.[system].pythoneda-sandbox-flow-sample-tests-python310` 
+- `#packages.[system].pythoneda-sandbox-flow-sample-tests-python311` 
---
+++ flake.nix
@@ -0,0 +1,248 @@
+# flake.nix
+#
+# This file packages pythoneda-sandbox/flow-sample-tests as a Nix flake.
+#
+# Copyright (C) 2023-today rydnr's pythoneda-sandbox-def/flow-sample-tests
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+{
+  description = "Tests for pythoneda-sandbox Flow Sample package";
+  inputs = rec {
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    nixos.url = "github:NixOS/nixpkgs/23.11";
+    pythoneda-shared-pythoneda-banner = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixos.follows = "nixos";
+      url = "github:pythoneda-shared-pythoneda-def/banner/0.0.40";
+    };
+    pythoneda-shared-pythoneda-domain = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixos.follows = "nixos";
+      inputs.pythoneda-shared-pythoneda-banner.follows =
+        "pythoneda-shared-pythoneda-banner";
+      url = "github:pythoneda-shared-pythoneda-def/domain/0.0.22";
+    };
+    pythoneda-sandbox-flow-sample = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixos.follows = "nixos";
+      inputs.pythoneda-shared-pythoneda-banner.follows =
+        "pythoneda-shared-pythoneda-banner";
+      inputs.pythoneda-shared-pythoneda-domain.follows =
+        "pythoneda-shared-pythoneda-domain";
+      url = "github:pythoneda-sandbox-def/flow-sample/0.0.4";
+    };
+    rydnr-testcontainers-python = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixos.follows = "nixos";
+      url = "github:rydnr/testcontainers-python/0.0.0";
+    };
+  };
+  outputs = inputs:
+    with inputs;
+    let
+      defaultSystems = flake-utils.lib.defaultSystems;
+      supportedSystems = if builtins.elem "armv6l-linux" defaultSystems then
+        defaultSystems
+      else
+        defaultSystems ++ [ "armv6l-linux" ];
+    in flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        org = "pythoneda-sandbox";
+        repo = "flow-sample-tests";
+        version = "0.0.1";
+        sha256 = "1j0wqndxrs976dyk1xyvs9zykm2nldqfacxc172zx9g5pjajrsmm";
+        pname = "${org}-${repo}";
+        pkgs = import nixos { inherit system; };
+        description = "Tests for pythoneda-sandbox Flow Sample package";
+        license = pkgs.lib.licenses.gpl3;
+        homepage = "https://github.com/pythoneda-sandbox/flow-sample-tests";
+        maintainers = [ "rydnr <github@acm-sl.org>" ];
+        archRole = "S";
+        space = "D";
+        layer = "D";
+        nixosVersion = builtins.readFile "${nixos}/.version";
+        nixpkgsRelease =
+          builtins.replaceStrings [ "\n" ] [ "" ] "nixos-${nixosVersion}";
+        shared = import "${pythoneda-shared-pythoneda-banner}/nix/shared.nix";
+        pythoneda-sandbox-flow-sample-tests-for = { python
+          , pythoneda-sandbox-flow-sample, rydnr-testcontainers-python }:
+          let
+            pnameWithUnderscores =
+              builtins.replaceStrings [ "-" ] [ "_" ] pname;
+            pythonpackage = "pythoneda_tests.sandbox.flows.sample";
+            pythonVersionParts = builtins.splitVersion python.version;
+            pythonMajorVersion = builtins.head pythonVersionParts;
+            pythonMajorMinorVersion =
+              "${pythonMajorVersion}.${builtins.elemAt pythonVersionParts 1}";
+            wheelName =
+              "${pnameWithUnderscores}-${version}-py${pythonMajorVersion}-none-any.whl";
+          in python.pkgs.buildPythonPackage rec {
+            inherit pname version;
+            projectDir = ./.;
+            pyprojectTemplateFile = ./pyprojecttoml.template;
+            pyprojectTemplate = pkgs.substituteAll {
+              authors = builtins.concatStringsSep ","
+                (map (item: ''"${item}"'') maintainers);
+              desc = description;
+              inherit homepage pname pythonMajorMinorVersion pythonpackage
+                version;
+              package = builtins.replaceStrings [ "." ] [ "/" ] pythonpackage;
+              pythonedaSandboxFlowSample =
+                pythoneda-sandbox-flow-sample.version;
+
+              pytest = python.pkgs.pytest.version;
+              src = pyprojectTemplateFile;
+            };
+            src = pkgs.fetchFromGitHub {
+              owner = org;
+              rev = version;
+              inherit repo sha256;
+            };
+
+            format = "pyproject";
+
+            nativeBuildInputs = with python.pkgs; [ pip poetry-core ];
+            propagatedBuildInputs = with python.pkgs; [
+              pythoneda-sandbox-flow-sample
+              pytest
+              rydnr-testcontainers-python
+            ];
+
+            pythonImportsCheck = [ pythonpackage ];
+
+            unpackPhase = ''
+              cp -r ${src} .
+              sourceRoot=$(ls | grep -v env-vars)
+              chmod +w $sourceRoot
+              cp ${pyprojectTemplate} $sourceRoot/pyproject.toml
+            '';
+
+            postInstall = ''
+              pushd /build/$sourceRoot
+              for f in $(find . -name '__init__.py'); do
+                if [[ ! -e $out/lib/python${pythonMajorMinorVersion}/site-packages/$f ]]; then
+                  cp $f $out/lib/python${pythonMajorMinorVersion}/site-packages/$f;
+                fi
+              done
+              popd
+              mkdir $out/dist
+              cp dist/${wheelName} $out/dist
+            '';
+
+            meta = with pkgs.lib; {
+              inherit description homepage license maintainers;
+            };
+          };
+      in rec {
+        defaultPackage = packages.default;
+        devShells = rec {
+          default = pythoneda-sandbox-flow-sample-tests-default;
+          pythoneda-sandbox-flow-sample-tests-default =
+            pythoneda-sandbox-flow-sample-tests-python311;
+          pythoneda-sandbox-flow-sample-tests-python38 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python38
+              }/bin/banner.sh";
+            extra-namespaces = "pythoneda_tests";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-sandbox-flow-sample-tests-python38;
+            python = pkgs.python38;
+            pythoneda-shared-pythoneda-banner =
+              pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python38;
+            pythoneda-shared-pythoneda-domain =
+              pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python38;
+            inherit archRole layer org pkgs repo space;
+          };
+          pythoneda-sandbox-flow-sample-tests-python39 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python39
+              }/bin/banner.sh";
+            extra-namespaces = "pythoneda_tests";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-sandbox-flow-sample-tests-python39;
+            python = pkgs.python39;
+            pythoneda-shared-pythoneda-banner =
+              pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python39;
+            pythoneda-shared-pythoneda-domain =
+              pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python39;
+            inherit archRole layer org pkgs repo space;
+          };
+          pythoneda-sandbox-flow-sample-tests-python310 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python310
+              }/bin/banner.sh";
+            extra-namespaces = "pythoneda_tests";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-sandbox-flow-sample-tests-python310;
+            python = pkgs.python310;
+            pythoneda-shared-pythoneda-banner =
+              pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python310;
+            pythoneda-shared-pythoneda-domain =
+              pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python310;
+            inherit archRole layer org pkgs repo space;
+          };
+          pythoneda-sandbox-flow-sample-tests-python311 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python311
+              }/bin/banner.sh";
+            extra-namespaces = "pythoneda_tests";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-sandbox-flow-sample-tests-python311;
+            python = pkgs.python311;
+            pythoneda-shared-pythoneda-banner =
+              pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python311;
+            pythoneda-shared-pythoneda-domain =
+              pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python311;
+            inherit archRole layer org pkgs repo space;
+          };
+        };
+        packages = rec {
+          default = pythoneda-sandbox-flow-sample-tests-default;
+          pythoneda-sandbox-flow-sample-tests-default =
+            pythoneda-sandbox-flow-sample-tests-python311;
+          pythoneda-sandbox-flow-sample-tests-python38 =
+            pythoneda-sandbox-flow-sample-tests-for {
+              python = pkgs.python38;
+              pythoneda-sandbox-flow-sample =
+                pythoneda-sandbox-flow-sample.packages.${system}.pythoneda-sandbox-flow-sample-python38;
+              rydnr-testcontainers-python =
+                rydnr-testcontainers-python.packages.${system}.rydnr-testcontainers-python-python38;
+            };
+          pythoneda-sandbox-flow-sample-tests-python39 =
+            pythoneda-sandbox-flow-sample-tests-for {
+              python = pkgs.python39;
+              pythoneda-sandbox-flow-sample =
+                pythoneda-sandbox-flow-sample.packages.${system}.pythoneda-sandbox-flow-sample-python39;
+              rydnr-testcontainers-python =
+                rydnr-testcontainers-python.packages.${system}.rydnr-testcontainers-python-python39;
+            };
+          pythoneda-sandbox-flow-sample-tests-python310 =
+            pythoneda-sandbox-flow-sample-tests-for {
+              python = pkgs.python310;
+              pythoneda-sandbox-flow-sample =
+                pythoneda-sandbox-flow-sample.packages.${system}.pythoneda-sandbox-flow-sample-python310;
+              rydnr-testcontainers-python =
+                rydnr-testcontainers-python.packages.${system}.rydnr-testcontainers-python-python310;
+            };
+          pythoneda-sandbox-flow-sample-tests-python311 =
+            pythoneda-sandbox-flow-sample-tests-for {
+              python = pkgs.python311;
+              pythoneda-sandbox-flow-sample =
+                pythoneda-sandbox-flow-sample.packages.${system}.pythoneda-sandbox-flow-sample-python311;
+              rydnr-testcontainers-python =
+                rydnr-testcontainers-python.packages.${system}.rydnr-testcontainers-python-python311;
+            };
+        };
+      });
+}
